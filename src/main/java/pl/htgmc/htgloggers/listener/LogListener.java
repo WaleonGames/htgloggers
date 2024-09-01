1adbcdcd05cd89d0748397393ffcd1f0c9b8fff2
WaleonGames <139130575+WaleonGames@users.noreply.github.com>
2024-09-01 12:49:28 +0200

    Update LogListener.java

--- src/main/java/pl/htgmc/htgloggers/listener/LogListener.java
+++ src/main/java/pl/htgmc/htgloggers/listener/LogListener.java
@@ -1,19 +1,29 @@
 package pl.htgmc.htgloggers.listener;
 
+import pl.htgmc.htgloggers.HTGLoggers;
 import pl.htgmc.htgloggers.events.LogEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class LogListener implements Listener {
 
+    private final HTGLoggers plugin; // Dodajemy kontekst pluginu
+
+    public LogListener(HTGLoggers plugin) {
+        this.plugin = plugin;
+    }
+
     @EventHandler
     public void onLogEvent(LogEvent event) {
         String message = event.getMessage();
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("plugins/LoggerPlugin/logs.txt", true))) {
+        File logFile = new File(plugin.getDataFolder(), "htgloggers-log.log");
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(logFile, true))) {
             writer.write(message);
             writer.newLine();
         } catch (IOException e) {
