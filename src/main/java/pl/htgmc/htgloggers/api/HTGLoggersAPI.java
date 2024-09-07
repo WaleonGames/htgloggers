d31d3fe0c193b96791611cdd517843338a3d5e4c
WaleonGames <139130575+WaleonGames@users.noreply.github.com>
2024-09-07 12:28:25 +0200

    Update HTGLoggersAPI.java

--- src/main/java/pl/htgmc/htgloggers/api/HTGLoggersAPI.java
+++ src/main/java/pl/htgmc/htgloggers/api/HTGLoggersAPI.java
@@ -7,29 +7,28 @@ import java.io.IOException;
 
 public class HTGLoggersAPI {
 
-    private static HTGLoggers plugin;
+    public static HTGLoggers plugin;
 
     public static void initialize(HTGLoggers plugin) {
         HTGLoggersAPI.plugin = plugin;
     }
 
-    public static File createLogFile(String fileName) {
-        File dataFolder = plugin.getDataFolder();
-        if (!dataFolder.exists() && !dataFolder.mkdirs()) {
-            plugin.getLogger().severe("Could not create data folder!");
-            return null;
+    public static void createLogFile(String pluginName) {
+        if (plugin == null) {
+            throw new IllegalStateException("HTGLoggersAPI.plugin is not set!");
         }
 
-        File logFile = new File(dataFolder, fileName);
+        File dataFolder = plugin.getDataFolder();
+        File logFile = new File(dataFolder, pluginName + "-log.log");
+
         try {
             if (!logFile.exists() && !logFile.createNewFile()) {
-                plugin.getLogger().severe("Could not create log file: " + fileName);
-                return null;
+                plugin.getLogger().severe("Could not create log file for plugin: " + pluginName);
+            } else {
+                plugin.getLogger().info("Log file created or loaded for plugin: " + pluginName);
             }
         } catch (IOException e) {
-            plugin.getLogger().severe("An error occurred while creating the log file: " + e.getMessage());
-            return null;
+            plugin.getLogger().severe("An error occurred while creating the log file for plugin " + pluginName + ": " + e.getMessage());
         }
-        return logFile;
     }
 }
