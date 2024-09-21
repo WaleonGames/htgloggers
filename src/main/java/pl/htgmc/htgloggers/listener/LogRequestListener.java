e628defaf98fc32521d063c4005221bb348de4cb
WaleonGames <139130575+WaleonGames@users.noreply.github.com>
2024-09-21 11:12:02 +0200

    Update LogRequestListener.java

--- src/main/java/pl/htgmc/htgloggers/listener/LogRequestListener.java
+++ src/main/java/pl/htgmc/htgloggers/listener/LogRequestListener.java
@@ -3,6 +3,7 @@ package pl.htgmc.htgloggers.listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import pl.htgmc.htgloggers.HTGLoggers;
+import pl.htgmc.htgloggers.api.HTGLoggersAPI;
 import pl.htgmc.htgloggers.events.LogRequestEvent;
 
 public class LogRequestListener implements Listener {
@@ -16,7 +17,13 @@ public class LogRequestListener implements Listener {
     @EventHandler
     public void onLogRequestEvent(LogRequestEvent event) {
         String pluginName = event.getPluginName();
-        plugin.createLogFile(pluginName); // Tworzenie pliku logu dla wskazanego pluginu
-        plugin.getLogger().info("Log file creation requested for plugin: " + pluginName);
+
+        try {
+            // Używanie API do tworzenia pliku logu
+            HTGLoggersAPI.createLogFile(pluginName);
+            plugin.getLogger().info("Log file creation requested for plugin: " + pluginName);
+        } catch (Exception e) {
+            plugin.getLogger().severe("Failed to create log file for plugin: " + pluginName + ". Error: " + e.getMessage());
+        }
     }
 }
